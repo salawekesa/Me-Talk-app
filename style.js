f0b8d430c9890c17a4f562b317d14667f8b63224
salawekesa <wekesa9235@gmail.com>
2023-01-26 17:34:16 +0300

    Added addFeeling() and selectors

--- style.js
+++ style.js
@@ -1,5 +1,49 @@
-function add(a=1,b=2){
-    return a+b;
+// selectors
+const alert = document.querySelector(".alert");
+const typeArea = document.querySelector(".typearea");
+const submit = document.querySelector(".btn-submit");
+const typed = document.getElementById("typed");
+const bodyArea = document.querySelector(".bodyarea");
+const feeling = document.querySelector(".feeling");
+const text = document.getElementById("textt");
+
+console.log("test");
+
+// editing 
+let editElement;
+let editOption = false;
+let editId = "";
+
+// Event listeners
+typeArea.addEventListener("submit", addfeeling);
+
+// Functions
+function addfeeling(e){
+    e.preventDefault();
+    const value = typed.value;
+
+    const id = new Date().getTime().toString();
+
+
+    if (value && !editOption) {
+        showAlerts("Feeling saved", "success");
+    }
+    else if(value && editOption){
+        console.log("editing");
+    }
+    else{
+        showAlerts("Feeling cant be empty", "danger");
+    }
+};
+
+// show alert - template strings
+function showAlerts(text,action){
+    alert.innerHTML = text;
+    alert.classList.add(`alert-${action}`);
 }
 
-console.log(add(undefined,undefined));
+// remove alert - callback fxn
+setTimeout(function(){
+    alert.innerHTML = "";
+    alert.classList.remove(`alert-${action}`);
+}, 5000);
